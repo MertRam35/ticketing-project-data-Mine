96d33f0344543dd09f6712ef9ba0f16335862cbc
MertRam <mert.ramadan35@gmail.com>
2023-10-31 16:08:15 +0300

    task mapper added

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,14 +1,25 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        return;
     }
 
     @Override
