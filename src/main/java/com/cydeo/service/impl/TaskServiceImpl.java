9427fb181f14cfa6f8d75915a0faebcfc7a9ac5c
MertRam <mert.ramadan35@gmail.com>
2023-10-31 16:00:56 +0300

    manual data.sql, task service and imp added

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.TaskService;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public List<TaskDTO> listAllTasks() {
+        return null;
+    }
+
+    @Override
+    public void save(TaskDTO dto) {
+
+    }
+
+    @Override
+    public void update(TaskDTO dto) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+        return null;
+    }
+}
